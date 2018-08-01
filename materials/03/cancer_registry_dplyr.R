49cded17a5cebac40c3c01717945d8c37f16583b
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2018-08-01 09:06:41 -0400

    Finalize session 03

--- materials/03/cancer_registry_dplyr.R
+++ materials/03/cancer_registry_dplyr.R
@@ -1,14 +1,11 @@
 library(tidyverse)
 library(readxl)
+library(readr)
 
 # Copied from RStudio's "Import Dataset..."
-cr_data <- read_excel("materials/03/CancerRegistryData.xlsx",
-                      col_types = c("numeric", "numeric", "text",
-                                    "text", "date", "numeric", "numeric",
-                                    "text", "text", "text", "text", "date",
-                                    "numeric", "text", "numeric", "text",
-                                    "text", "text", "text", "text", "text"))
-
-cr_patients <- read_excel("materials/03/CancerRegistryDataPatients.xlsx",
+cr_patients <- read_excel("CancerRegistryDataPatients.xlsx",
                           col_types = c("numeric", "numeric", "text",
                                         "text", "text", "text", "date", "text"))
+
+cr_patients <- read_csv("CancerRegistryDataPatients.csv",
+                        col_types = cols(`Date Last Patient Contact/Dead` = col_date(format = "%m/%d/%y")))

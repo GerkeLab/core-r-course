f2d369d4ee01f731022f971f8121b6ce22021410
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2018-08-21 16:09:24 -0400

    Initialize session 6

--- serve-site.R
+++ serve-site.R
@@ -1,13 +1,16 @@
 #!/usr/bin/env Rscript
 setwd(here::here())
+library(tictoc)
 
 render <- function(x) {
-  cli::cat_line(x)
+  tic("Update complete")
+  cli::cat_bullet("Update started:  ", x, col = "yellow", bullet_col = "yellow")
   if (any(grepl("slides\\.Rmd$", x))) {
     purrr::walk(x[grepl("slides\\.Rmd$", x)], rmarkdown::render)
-    rmarkdown::render_site()
-  } else rmarkdown::render_site()
+    rmarkdown::render_site(quiet = TRUE)
+  } else rmarkdown::render_site(quiet = TRUE)
+  cli::cat_bullet(capture.output(toc()), col = "green", bullet_col = "green")
 }
 
-rmarkdown::render_site()
+render("index.Rmd")
 servr::httw(handler = render, pattern = "\\.(R?md|yml|css)", host = "127.0.0.1")
